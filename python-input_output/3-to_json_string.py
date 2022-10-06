b54e1a4ce405001d0a6cb2992677d1190eda5d06
Cygnus-8 <muchrismonfort@gmail.com>
2022-10-06 06:59:43 -0700

    Task3: Feature added

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+To JSON String
+"""
+
+
+import Json
+
+def to_json_string(my_obj):
+    """
+    Function that returns the Json representation
+    of an object
+    """
+    return(json.dumps(my_obj))
