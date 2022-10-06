d979aea5f75d707a4b4ee3f167f697c37a9ae7b2
Cygnus-8 <muchrismonfort@gmail.com>
2022-10-06 07:53:15 -0700

    Task 6: Feature added

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -11,7 +11,7 @@ def load_from_json_file(filename):
     Function that creates an object from a text file
     using a JSON String
     """
-    with open(filename, "w") as f:
+    with open(filename, "r") as f:
         str = json.load(f)
     f.close()
     return (str)
