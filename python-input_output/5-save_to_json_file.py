0664005023818f38d408e9af1ac19f73e2980951
Cygnus-8 <muchrismonfort@gmail.com>
2022-10-06 07:33:13 -0700

    Task 5: Feature added

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -11,7 +11,6 @@ def save_to_json_file(my_obj, filename):
     Function that writes an object to a text file
     using a JSON representation
     """
-    str = json.dumps(my_obj)
     with open(filename, "w") as f:
-        f.write(str)
+        json.dump(my_obj, filename)
     f.close()
